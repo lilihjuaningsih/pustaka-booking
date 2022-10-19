d90d33e13b99b0b9d4f7ba5901b228da961cb14a
lilihjuaningsih <lilihjuaningsih06@gmail.com>
2022-10-19 20:42:40 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Lilih
+            saya tinggal di daerah karawang
+            saya suka tidur ";
+    }
+}
